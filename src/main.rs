ab9f86c4ada12fbd2bd8a7867abb7f93bb08ca15
GHOSt <ghostdevbusiness@gmail.com>
2022-05-11 15:07:43 +0100

    feat: cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+    #[clap(short, long)]
+    matcher: String,
+
+    #[clap(short, long, default_value = "")]
+    has: String,
+
+    #[clap(short, long, default_value = "")]
+    exclude: String
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let word_list = fs::read_to_string("words.txt").expect("Failed to read words");
+    let words: Vec<&str> = word_list.split("\n").collect();
+
+    let regex = regex::Regex::new(&args.matcher).unwrap();
+
+    println!("Matches:");
+
+    for word in words {
+        if regex.is_match(word) {
+            if !args.has.is_empty() {
+                let has_regex = regex::Regex::new(&format!("[{}]", args.has)).unwrap();
+
+                if !has_regex.is_match(word) {
+                    continue;
+                }
+            }
+            
+            if !args.exclude.is_empty() {
+                let exclude_regex = regex::Regex::new(&format!("[{}]", args.exclude)).unwrap();
+
+                if exclude_regex.is_match(word) {
+                    continue;
+                }
+            }
+            
+            println!("{}", word);
+        }
+    }
 }
